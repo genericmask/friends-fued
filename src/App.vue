b6a2da277ae3ae783020f10d504469d604874c7b
Casey Feikema <feikema.casey@gmail.com>
2024-06-22 22:17:49 -0400

    init 2

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <!-- <header>
+
+    <div>
+
+      <nav>
+        <RouterLink to="/">Game Board</RouterLink>
+      </nav>
+    </div>
+  </header> -->
+
+  <RouterView />
+</template>
+
+<style scoped>
+</style>
