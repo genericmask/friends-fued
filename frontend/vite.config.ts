afb4dc1e9b6348d85f0e4d921c11667e1cc37bfd
Casey Feikema <feikema.casey@gmail.com>
2024-09-26 10:41:34 -0400

    frontend now works through gateway

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,12 +5,14 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    host: '0.0.0.0', // 0.0.0.0 ensures that the dev server listens on all network interfaces
+    port: 5173
   }
 })
